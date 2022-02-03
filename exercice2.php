be6301e4942ab7fce6510a427387b79937a4d8f9
mona21000 <98873477+mona21000@users.noreply.github.com>
2022-02-03 20:57:41 +0200

    addition

--- exercice2.php
+++ exercice2.php
@@ -1,6 +1,14 @@
+
 <?php
+# 1. Write PhP script with two string variables. Assign any text to these variables. Join them together. Print the length of the string.(Hint: string function) 
+
 $text1="I like";
 $text2="programming";
 echo strlen ($text1 . " " . $text2);
 
+# 2. In your above code, Change the double quotes to single quotes.Then run the script again. Did it have any effect?
+
+$text1='I like';
+$text2='programming';
+echo strlen ($text1 . " " . $text2);
 ?>
