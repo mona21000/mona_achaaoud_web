ce824ecd9a116d0d8ff34dcbfa90db35b644a066
mona21000 <98873477+mona21000@users.noreply.github.com>
2022-02-06 14:50:28 +0200

    num1

--- exercise3.php
+++ exercise3.php
@@ -1,3 +1,20 @@
 <?php
 echo"<h1>Exercice 3 - Mona Achaaoud</h1>";
+
+/*
+1. Write a script that gets the current month and prints one of the following responses, depending on whether it's August or not:
+
+It's August, so it's still holiday.
+Not August, This is Month-name so i don't have any holidays
+
+You can use date(F) function to get the current month name
+*/
+$m= date("M");
+if ($m == "August") {
+    echo("It's August, so it's still holiday.");
+else {
+    echo("Not August, This is $m so i don't have any holidays.");
+}
+
+}
 ?>
